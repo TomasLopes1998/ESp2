01d28e6fa141d7bcce78d4cef23fbf19b62c210a
AAMF1 <alex.amf17@gmail.com>
2020-05-22 16:47:52 +0100

    updates login e menuprincipal

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import MenuPrincipal from "./MenuPrincipal";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MenuPrincipal />
   </React.StrictMode>,
   document.getElementById("root")
 );
@@ -15,3 +16,4 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+//<App />
