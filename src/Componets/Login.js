7066924c12cd666b08065001a8330556377c90de
TomasLopes1998 <44590993+TomasLopes1998@users.noreply.github.com>
2020-05-24 20:25:33 +0100

    Just cleaning the project

--- src/Componets/Login.js
+++ src/Componets/Login.js
@@ -77,11 +77,11 @@ class Page extends React.Component {
 
     render() {
         console.log(this.state.token)
-        if (this.state.errormessage != undefined)
+        if (this.state.errormessage !== undefined)
             var errormessage = <h2>{this.state.errormessage}</h2>
         else
-            var errormessage = <div/>
-        if (this.state.token == undefined)
+            errormessage = <div/>
+        if (this.state.token === undefined)
             return (
                 <div>
                     <h1>Welcome to ESUber</h1>
